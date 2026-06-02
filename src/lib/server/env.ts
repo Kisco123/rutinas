7f427f10bb5e77e384e849d76493e2207d04abc1
Francisco <franciscohubner12@gmail.com>
2026-06-02 19:53:12 -0400

    chore: add env config and example

---
+++ src/lib/server/env.ts
@@ -0,0 +1,11 @@
+import {
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_ANON_KEY
+} from '$env/static/public';
+import { SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private';
+
+export const env = {
+  supabaseUrl: PUBLIC_SUPABASE_URL,
+  supabaseAnonKey: PUBLIC_SUPABASE_ANON_KEY,
+  supabaseServiceRoleKey: SUPABASE_SERVICE_ROLE_KEY
+};
